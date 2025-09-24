ea6ea272cc49f8e539f397bffb0e9f7c1d1b3803
imadhn961 <imad.hassan.0961@gmail.com>
2025-09-25 01:31:37 +0300

    Build Hive and Create noteModel generate with Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:nots/Views/HomePage.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox("Notes");
   runApp(const MyApp());
 }
 
